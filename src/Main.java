c75e9f71aafeed90fb8255c15d582be89d454d53
taek2222 <oot309@damu.net>
2023-01-12 22:44:40 +0900

    Test

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int lottonumber = 1;
+
+        while (true) {
+            System.out.println("--------------- LOTTO 프로그램 ---------------");
+            System.out.println("\t\t\t  [ 제 " + lottonumber + "회 로또 추첨 ]");
+            System.out.println("\t\t\t  [ 제 " + lottonumber + "회 로또 추첨 ]");
+        }
+    }
+}
