12feacf31566658564908aeff89b8720de2c6a21
taek2222 <oot309@daum.net>
2023-01-14 00:25:32 +0900

    01.14

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,18 @@
 import money.Money;
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Money doll = new Money();
-        DecimalFormat df = new DecimalFormat("###,###"); // 숫자 천 단위 , 적용 클래스
         Scanner sc = new Scanner(System.in);
 
         int lottonumber = 1;
         int age;
-        String Moneyprint;
 
         while (true) {
             System.out.println("--------------- LOTTO 프로그램 ---------------");
             System.out.println("\t\t\t  [ 제 " + lottonumber + "회 로또 추첨 ]");
-            Moneyprint = df.format(doll.money); // int > string 숫자 천 단위로 변환
-            System.out.println("\t\t\t  현재 소지금 : " + Moneyprint);
+            doll.moneyprint();
             System.out.println("---------------------------------------------");
 
             System.out.println("[ LOTTO 프로그램 기능을 선택해주세요. ]");
@@ -29,6 +25,10 @@ public class Main {
             switch (age){
                 case 1:
                 case 2:
+                case 3:
+                case 4:
+                default:
+                    System.out.println("번호를 잘못 입력하였습니다. 다시 입력바랍니다.");
             }
             break;
         }
--- src/money/Money.java
+++ src/money/Money.java
@@ -1,6 +1,20 @@
 package money;
 
-public class Money {
+import java.text.DecimalFormat;
 
-    public int money = 5000; // 기본 소지금 $ 5,000원
+public class Money {
+    DecimalFormat df = new DecimalFormat("###,###"); // 숫자 천 단위 , 적용 클래스
+    public int money;// 기본 소지금 $ 5,000원
+    String Moneyprint;
+    public Money() { money = 5000; }
+
+    public void moneyprint() // 현재 금액 출력 객체
+    {
+        Moneyprint = df.format(getmoney()); // int > string 숫자 천 단위로 변환
+        System.out.println("\t\t\t  현재 소지금 : " + Moneyprint);
+    }
+
+    public void setmoney(int money) { this.money = money; } // 머니 값 변경 객체
+    public int getmoney() { return money; }
+
 }
